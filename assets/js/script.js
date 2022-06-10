5f55347552143c77ad6a3e31e3b4a6e7d6e3f16d
MartinaP87 <bleach1987@hotmail.it>
2022-06-10 17:30:11 +0000

    Exercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,50 @@
-let items = ["A","B","C"];
-let obj = {
-[items]: "Hello"
+
+
+
+let dictionary = [["lots", "1"], ["of", "2"], ["words", "3"], ["from", "4"], 
+["vocabulary", "5"], ["picked", "6"], ["by", "7"], ["me", "8"]]
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function pickWord () {
+    let ind1 = Math.floor(Math.random() * 7) ;
+    return dictionary[ind1][0]
 }
-console.log(obj); // A,B,C: "Hello"
-console.log(obj["A,B,C"]); // Hello
-console.log(items[0]); // A
+let pickedWord = pickWord()
+pickWord()
+document.getElementById("game-area").innerText = "<p>bo</p>";
+/**
+ * function to hide the letters of the picked world and
+ * display the letter when guessed right
+ */
+function hide() {
+   
+        // document.getElementById(sub-game-area).innerText = "_ ";
+       let boxes = document.createElement("div");
+       boxes.className = "letters";
+
+       document.getElementById("game-area").appendChild(boxes);
+    }
+    hide()
+
+for (let i = 0; i < pickedWord.length; i++) 
+document.getElementsByClassName("letters").innerText = 
+
+let playground = document.getElementsByTagName("body")
+playground.addEventListener("onkeyup", show)
+function show(event){
+    for (i = 0; i < pickedWord.length; i++) {
+    if (event.key === pickedWord[i]) {
+
+    }
+ } else {
+
+    }
+}
+
+function runGame() 
+function incrementScore() 
+function incrementWrongAnswer() {
+
+}
