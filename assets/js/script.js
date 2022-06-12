398ed013bb0ac0c6632fb7d09ba3c2615b222bcf
MartinaP87 <bleach1987@hotmail.it>
2022-06-12 18:57:20 +0000

    vittoria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-
 let dictionary = [["lots", "1"], ["of", "2"], ["words", "3"], ["from", "4"], 
 ["vocabulary", "5"], ["picked", "6"], ["by", "7"], ["me", "8"]]
 /**
- * The main game "loop", called when the script is first loaded
+ * The first game function, called when the script is first loaded
  * and after the user's answer has been processed
  */
 function pickWord () {
@@ -11,14 +10,17 @@ function pickWord () {
 }
 let pickedWord = pickWord();
 pickWord();
+
+console.log(pickedWord);
+
 /**
- * create divs in base of the number of letters of the picked word 
+ * Create divs in base of the number of letters with their respective letters in them.
  */
  let boxes = '';
  for (let i = 0; i < pickedWord.length; i++) {
      boxes += `
-     <div class="letters ${i}">
-     ${pickedWord[i]}
+     <div class="container">
+     <h2 class="${i}">${pickedWord[i]}</h2>
      </div>
      `;
    
@@ -26,116 +28,46 @@ pickWord();
  console.log(boxes);
 document.getElementById("game-area").innerHTML = boxes;
 
+/**
+ * This function shows the letter in its position if correct 
+ * and call change the function incrementWrongAnswer if wrong
+ */
+ let letters = document.getElementsByTagName("h2");
+ let letterContainer = document.getElementsByClassName("container")
 
+function keyPressed(event) {
+    for (i = 0; i < pickedWord.length; i++) {
 
+       if (pickedWord.includes(event.key)) { 
+          console.log("true");
+          if (event.key === pickedWord[i]) { 
+          letters[i].style.visibility = "visible";
+          letterContainer[i].style.backgroundColor = "yellow"
+          }
+        } else {
+        incrementWrongAnswer()
+        }
+    }
+};
+// This function change the image every time the answer is wrong
+function incrementWrongAnswer() {
+    console.log("wrong")
+};
 
-//for (let i = 0; i < pickedWord.length; i++) 
-//document.getElementsByClassName("letters").innerText = 
+function victory(event) {
+    let results = [];
+    for (i = 0; i < letters.length; i++) {
+        
+        if (letterContainer[i].style.backgroundColor === "yellow"){
+            results.push("true")
+        } else { 
+            results.push("false")
+            }
 
-////body.addEventListener("onkeyup", show);
-
-function show(event) 
-  // for (i = 0; i < pickedWord.length; i++) 
-   // if (event.key === pickedWord[i])  
-
-    
-
-function runGame() 
-function incrementScore() 
-function incrementWrongAnswer()
-
-// Create an empty array: 
-
-let numbers = []; 
-
-  
-
-// Put 1000 random numbers in it, between 0 and 1000 (exclusive of 1000, and duplicates are fine...it's random) 
-
-let i = 0; 
-
-while (i < 1000) { 
-
-    numbers.push(Math.floor(Math.random() * 1000)); 
-
-    i++; 
-
-}; 
-
-  
-
-// create a table element. Give it an ID so we can identify it 
-
-let table = document.createElement('table'); 
-
-table.id = 'random-numbers'; 
-
-  
-
-// Create some basic HTML markup for the table structure: 
-
-let tableHtml = ` 
-
-<thead> 
-
-    <tr> 
-
-        <td>Item #</td> 
-
-        <td>Value</td> 
-
-    </tr> 
-
-</thead> 
-
-<tbody> 
-
-</tbody> 
-
-`; 
-
-  
-
-// Set the table's inner html 
-
-table.innerHTML= tableHtml; 
-
-  
-
-// Now it's an HTMLElement object, we can get its body! 
-
-let tbody = table.getElementsByTagName('tbody')[0]; 
-
-  
-
-// Iterate the array and append a row to the table body's html for each number. This is using template literals again 
-
-let tbodyHtml = ''; 
-
-for (let i = 0; i < numbers.length; i++) { 
-
-    tbodyHtml += ` 
-
-        <tr> 
-
-            <td>${i}</td> 
-
-            <td>${numbers[i]}</td> 
-
-        </tr> 
-
-    ` 
-
-} 
-
-  
-
-// With iteration complete, we now have a long string of HTML w/ all the rows for the table body...just set it! 
-
-tbody.innerHTML = tbodyHtml; 
-
-  
-
-// Append the whole table to the document's body...voila! 1000 rows! 
-
-document.body.appendChild(table); 
+            console.log(results)
+            
+        if (results.length === letters.length && results[i] === "true") {
+                console.log("ok")
+            }
+        }
+    };
