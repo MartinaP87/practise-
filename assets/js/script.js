6e04e7be6aa9418f1502cbf00ccc47827fafe820
MartinaP87 <bleach1987@hotmail.it>
2022-06-14 10:40:52 +0000

    ancora no

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,10 @@ let dictionary = [["lots", "1"], ["of", "2"], ["words", "3"], ["from", "4"],
 function pickWord () {
     let ind1 = Math.floor(Math.random() * 7) ;
     return dictionary[ind1];
-}
+};
 let pickedWord = pickWord()[0];
 let pickedWordMeaning = pickWord()[1];
+
 pickWord();
 
 console.log(pickedWord);
@@ -35,29 +36,32 @@ document.getElementById("game-area").innerHTML = boxes;
  * and call change the function incrementWrongAnswer if wrong
  */
  let letters = document.getElementsByTagName("h2");
- let letterContainer = document.getElementsByClassName("container")
+ let letterContainer = document.getElementsByClassName("container");
+ let doomed = document.getElementById("theman");
+ let messageBox = document.getElementById("final-message");
 
  function keyPressed(event) {
-    
-   
       if (pickedWord.includes(event.key)) { 
           console.log("true");
           for (i = 0; i < pickedWord.length; i++) {
-             
-          if (event.key === pickedWord[i]) { 
+             if (event.key === pickedWord[i]) { 
              letters[i].style.visibility = "visible";
              letterContainer[i].style.backgroundColor = "yellow"
-             }
-          } 
-       } else {
-         incrementWrongAnswer()
+             } 
+            }; 
+          victory();  
+        } else {
+         incrementWrongAnswer();
+        };
+
+      if (messageBox.style.backgroundColor === "aqua") {
+            console.log("bastaaaa");
+        
          }
         };
 
 
-
-let doomed = document.getElementById("theman");
-// This function change the image every time the answer is wrong
+// This function change the image every time the answer is wrong 
 var n = 0;
 function incrementWrongAnswer() {
     
@@ -67,42 +71,38 @@ function incrementWrongAnswer() {
      if ( n === 6) {
          
          console.log("wrong");
-         
-         document.getElementById("final-message").innerHTML = `
+         messageBox.style.backgroundColor = "aqua";
+         messageBox.innerHTML = `
         <h3>Oh nooo!</h3>
         <h3>You haven't found the word this time... but, if you learn from a loss you have not lost!</h3>
         <h3>The word was:</h3>
         <h4><strong>${pickedWord}</strong>: <em>${pickedWordMeaning}</em></h4>
         `;
-
-         }
-   
-    console.log(n);   
-    
+         return;
+         } 
 };
 
+// Define when the user wins 
 
-
-function victory(event) {
+function victory() {
   let results = [];
-    for (i = 0; i < letters.length; i++) {
-        
+    for (i = 0; i < letters.length; i++) { 
         if (letterContainer[i].style.backgroundColor === "yellow"){
             results.push("true")
         } else { 
             results.push("false")
         }
-
      console.log(results)
             
     if (results.length === letters.length && results[i] === "true") {
         console.log("ok");
-        doomed.style.backgroundImage = `url(assets/images/imgvic.png)`;
-        document.getElementById("final-message").innerHTML = `
+        doomed.style.backgroundImage = "url(assets/images/imgvic.png)";
+        messageBox.style.backgroundColor = "aqua";
+        messageBox.innerHTML = `
         <h3>Congratulations! You found the word!</h3>
         <h4><strong>${pickedWord}</strong>: <em>${pickedWordMeaning}</em></h4>
         `;
-        doomed.style.backgroundImage = "none";
-        }
-      }
+        return;
+        } 
+      } 
     };
