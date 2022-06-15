2e8686fdba9961ccda2752d0140d7624e31d9a42
MartinaP87 <bleach1987@hotmail.it>
2022-06-15 13:42:02 +0000

    style

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,39 +32,47 @@ let boxes = '';
 for (let i = 0; i < pickedWord.length; i++) {
   boxes += `
      <div class="container">
-     <h2>${pickedWord[i]}</h2>
+     <h2 class="word-letters">${pickedWord[i]}</h2>
      </div>
      `;
 }
-console.log(boxes);
-document.getElementById("game-area").innerHTML = boxes;
+document.getElementById("boxes-area").innerHTML = boxes;
 
 /**
  * This function shows the letter in its position if correct 
  * and call change the function incrementWrongAnswer if wrong
  */
-let letters = document.getElementsByTagName("h2");
-let letterContainer = document.getElementsByClassName("container");
+let inputBox = document.getElementById("input-box");
+var inputLettersList = [];
 let doomed = document.getElementById("theman");
+let letterContainer = document.getElementsByClassName("container");
+let letters = document.getElementsByClassName("word-letters");
 let messageBox = document.getElementById("final-message");
 
 document.body.addEventListener("keypress", keyPressed);
 
 function keyPressed(event) {
+
+  // Add key pressed in the input box. 
+  inputLettersList.push(`  ${event.key}`);
+  inputBox.innerHTML= `
+        <h2>Letters tried:</h2>
+        <h3> ${inputLettersList}</h3>
+        `;
+
+  //Check if the key pressed is included in the picked word; 
+  //if it is reveal it and trigger the victory fuction otherwise start to build the hangman;
   if (pickedWord.includes(event.key)) {
     console.log("true");
     for (i = 0; i < pickedWord.length; i++) {
-      if (event.key === pickedWord[i] && messageBox.style.backgroundColor !== "aqua") {
+      if (event.key === pickedWord[i]) {
         letters[i].style.visibility = "visible";
-        letterContainer[i].style.backgroundColor = "yellow"
-        console.log("primo loop lettere visibili");
-      } else if (messageBox.style.backgroundColor === "aqua") {
-       break;
-      }
-    };
+        letterContainer[i].style.backgroundColor = "yellow"   
+      } 
+    }
     victory();
   } else {
-    incrementWrongAnswer();
+    incrementWrongLetter();
   };
 
   if (messageBox.style.backgroundColor === "aqua") {
@@ -74,25 +82,22 @@ function keyPressed(event) {
   }
 };
 
-
 // This function change the image every time the answer is wrong 
 var n = 0;
-
-function incrementWrongAnswer() {
-
+function incrementWrongLetter() {
   if (n < 6) {
     n++;
     doomed.style.backgroundImage = `url(assets/images/img${n}.png)`;
   }
   if (n === 6) {
-
     console.log("wrong");
     messageBox.style.backgroundColor = "aqua";
     messageBox.innerHTML = `
         <h3>Oh nooo!</h3>
         <h3>You haven't found the word this time... but, if you learn from a loss you have not lost!</h3>
         <h3>The word was:</h3>
-        <h4><strong>${pickedWord}</strong>: <em>${pickedWordMeaning}</em></h4>
+        <p><strong>${pickedWord}</strong>: <em>${pickedWordMeaning}</em></p>
+        <a href="index.html">Try Again!</a>
         `;
   }
 };
@@ -107,16 +112,14 @@ function victory() {
     } else {
       results.push("false")
     }
-    console.log(results);
-    
-
     if (results.length === letters.length && results[i] === "true") {
       console.log("ok");
       doomed.style.backgroundImage = "url(assets/images/imgvic.png)";
       messageBox.style.backgroundColor = "aqua";
       messageBox.innerHTML = `
         <h3>Congratulations! You found the word!</h3>
-        <h4><strong>${pickedWord}</strong>: <em>${pickedWordMeaning}</em></h4>
+        <p><strong>${pickedWord}</strong>: <em>${pickedWordMeaning}</em></p>
+        <a href="index.html">Try Again!</a>
         `;
     }
   }
